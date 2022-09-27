2871d8d2944751791197b766c8b6f1285269b455
Sanjeev <asanjiv1250@gmail.com>
2022-09-28 01:01:20 +0530

    add new files

--- file2.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-extern int a;
-void fun();
-{
-	printf("%d",a);
-	a=200;
-}
--- undef.c
+++
@@ -1,6 +0,0 @@
-    #include <stdio.h>  
-    #define PI 3.14  
-    #undef PI  
-    main() {  
-       printf("%f",PI);  
-    }  
